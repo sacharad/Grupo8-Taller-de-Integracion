721c1095d272276cda44cd66fc047f56430ae2b1
Natsch <natsch@natsch-Inspiron-5421.(none)>
2014-04-09 11:31:43 -0400

    ecommerce

---
+++ ecommerce/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecommerce::Application.config.secret_key_base = '5344a130b3c7ee2ef0fac149e047e97bc7c32902c8dfe120e2f590808c37b0b88f2d23d2d0adddbbcc4e2a7584eda0aaa39cad0fd6b7441fbd2b8c17e32594a3'
