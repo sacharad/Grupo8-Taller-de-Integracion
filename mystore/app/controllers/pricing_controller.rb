1bad4acf25b6a2fee31d20f1af44c551fa8437e0
niescobar <the_realgunner@hotmail.com>
2014-05-09 08:10:33 -0400

    Conexion con dropbox lista

--- mystore/app/controllers/pricing_controller.rb
+++ mystore/app/controllers/pricing_controller.rb
@@ -6,11 +6,13 @@ class PricingController < ApplicationController
 
 		# Asks for authorization to Dropbox's account for access from SmartBoard to the files on it repository.
 	def authorize
-
-		session[:return_to] = request.referer
+		@dbsession = nil
+		@@dropbox_session = nil
+		#session[:return_to] = request.referer
 		@dbsession = DropboxSession.new(DROPBOX_APP_KEY, DROPBOX_APP_KEY_SECRET)
+		puts DROPBOX_APP_KEY, DROPBOX_APP_KEY_SECRET
 		#serialize and save this DropboxSession
-		session[:dropbox_session] = @dbsession.serialize
+		@@dropbox_session = @dbsession.serialize
 		#pass to get_authorize_url a callback url that will return the user here
 		redirect_to @dbsession.get_authorize_url url_for(:action => 'callback')
 	end
@@ -20,47 +22,43 @@ class PricingController < ApplicationController
 	# @Return : None
 	# @Purpose : To callback for Dropbox authorization
 	def callback
-		@dbsession = DropboxSession.deserialize(session[:dropbox_session])
+		
+		@dbsession = DropboxSession.deserialize(@@dropbox_session)
 		@dbsession.get_access_token #we've been authorized, so now request an access_token
-		session[:dropbox_session] = @dbsession1.serialize	
+		@dropbox_session = @dbsession.serialize
 
 		flash[:success] = "You have successfully authorized with dropbox."
 
-		#redirect_to session[:return_to]	
+		read_prices(DropboxSession.deserialize(@dropbox_session))
+
 		redirect_to services_show_path
-		#redirect_to root_path
-		
-	rescue 
-		session[:dropbox_session] = nil
-	    flash[:success] = "Failed to authorize"
+
+	#rescue 
+		#session[:dropbox_session] = nil
+	    #flash[:success] = "Failed to authorize"
+		#redirect_to services_show_path
+
 		#redirect_to session[:return_to]	
-		redirect_to services_show_path
 	end # end of dropbox_callback action
 
 
-	def read_prices
-		
-	    if session[:dropbox_session]  
-		      	
-		      	@dropbox_token = session[:dropbox_session]
-		      	session.delete :dropbox_session 
-				#authorize
-
+	def read_prices(db_token)    
+		      	   		      	
+		      
+				@dropbox_token = db_token
 				client = DropboxClient.new(@dropbox_token)
-				contents, metadata = client.get_file_and_metadata('/Documentos/Franco Escobar.png')
-				open('/Documentos/Franco Escobar.png', 'w') {|f| f.puts contents }
-				redirect_to services_show_path
-		end 
+				contents, metadata = client.get_file_and_metadata('Grupo8/DBPrecios.accdb')
+				begin
+				      open('DBPrecios.accdb', 'wb') {|f| f.puts contents }
+				   
+				     
+				rescue
+				      flash[:success] =  "Exception occured while downloading..."		
+
+	    		end 	  
+
 	end
 
-	def create
-  # some object you want to create
-  # if the object.save is fine
-  #   redirect_to object
-  # else
-  #   render new with the errors
-  # end
-	end
 
 end
 
--- mystore/config/initializers/dropbox_config.rb
+++ mystore/config/initializers/dropbox_config.rb
@@ -1,3 +1,3 @@
-DROPBOX_APP_KEY = "fyofw56x8jeu6ko"
-DROPBOX_APP_KEY_SECRET = "5ekp9gxh3lmnww9"
-DROPBOX_APP_MODE = "sandbox"
+DROPBOX_APP_KEY = "tp86czomsmymmam"
+DROPBOX_APP_KEY_SECRET = "h10peq4yyh5s34o"
+DROPBOX_APP_MODE = "dropbox"
