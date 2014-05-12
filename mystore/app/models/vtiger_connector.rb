b94a70df4c6d2919463371d9cb9be613c9e9cb6d
acathali <acathali@uc.cl>
2014-05-12 13:39:45 -0400

    conexion a Vtiger y metodo get_address

--- mystore/app/models/vtiger_connector.rb
+++ mystore/app/models/vtiger_connector.rb
@@ -1,31 +1,25 @@
-class VtigerConnector < Connectors::BaseConnector
+require 'uri'
 
-  def ininitialize
+class VtigerConnector
+
+  def initialize
     access_key = '9Q7SnRuMMZz6ZVP2'
-    url = 'http://integra.ing.puc.cl/vtigerCRM/webservice.php?'
+    @url = 'http://integra.ing.puc.cl/vtigerCRM/webservice.php?'
 
-    challenge = JSON.parse(open("#{url}operation=getchallenge&username=grupo8").read)
+    challenge = JSON.parse(open("#{@url}operation=getchallenge&username=grupo8").read)
     token = challenge["result"]["token"]
 
-    @response = RestClient.post( url , { 
+    response = RestClient.post( @url , { 
       'operation'=>'login', 
       'username' => 'grupo8', 
       'accessKey' => Digest::MD5.hexdigest("#{token}#{access_key}")   
     })
+
+    @session_id = JSON.parse(response)["result"]["sessionName"]
   end
 
-  def get_address(client_id)
-    
-  end
-
-#####TEMP#####
-  def data
-    contact_query = URI.encode("SELECT * FROM contacts LIMIT 10;")
-    session_id = JSON.parse(@response)["result"]["sessionName"]
-    @contactos = RestClient.get( url , {
-      'operation' => 'query',
-      'sessionName' => session_id,
-      'query' => contact_query
-    })
+  def get_address(client_cf_705)
+    query = URI.encode("SELECT bill_street, bill_city, bill_state FROM Accounts WHERE cf_705 = '#{client_cf_705}';")
+    address = RestClient.get("#{@url}operation=query&sessionName=#{@session_id}&query=#{query}")
   end
 end
