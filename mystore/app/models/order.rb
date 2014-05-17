5342d96468295f7aaaad03c8525e89584269b1ef
Nicols Escobar T <nicolasescobar@ubuntu.(none)>
2014-05-17 10:46:38 -0400

    Inserciones en mongo ok

--- mystore/app/models/order.rb
+++ mystore/app/models/order.rb
@@ -11,23 +11,45 @@ class Order < ActiveRecord::Base
 	def self.insert_report (collection_name, report)
 		#collection_name tiene el nombre de la coleccion
 		#reporte corresponde al archivo que trae el reporte
-
 		mongo_client = MongoClient.new("localhost", 27017)
-		db     = mongo_client.db("mongodb")
-		coll = db.collection(collection_name)
-		coll.insert(reporte)
-
-	end
-
-	def self.report_order(info)
-		insert_report("InfoPedidos",info)		 
-	end
-
-	def self.report_sales
+		@db     = mongo_client.db("mongodb")
+		coll = @db.collection(collection_name)
+		coll.insert(report)
 		
 	end
 
-	def self.report_BrokeStock
+	#example info: {"fecha" => "12/03/2013", "hora" => "16:00", "rut" =>"171234560", "producto" => {"sku" => "US123", "cantidad" => 3}}
+	def self.report_order(info)
+		insert_report("InfoPedidos",info)		 
+		
+	end
+
+	#example sale: {"rut" =>"151234560", "fecha" => "13/06/2014", "hora" => "10:01", "precio_venta" => "52000", "producto" => {"sku" => "SKU45123", "cantidad" => 10}}
+	def self.report_sales(sale)
+		insert_report("Reporte_Ventas",sale)
+		
+	end
+
+	def self.report_BrokeStock(quiebre)
+		insert_report("Reporte_QuiebresStock",sale)
+	end
+
+
+
+	##Metodos internos solo para probar los insert en Mongo
+
+	def self.list_collection
+
+		mongo_client = MongoClient.new("localhost", 27017)
+		db     = mongo_client.db("mongodb")
+		db.collection_names
+	end 
+
+	def self.get_collection(name)
+
+		mongo_client = MongoClient.new("localhost", 27017)
+		@db     = mongo_client.db("mongodb")
+		coll = @db.collection(name)
 
 	end
 	
