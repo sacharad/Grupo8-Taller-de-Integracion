be84140cf9a94270a422c61509bbf516b5262c1d
Natsch <natsch@natsch-Inspiron-5421.(none)>
2014-05-18 20:03:45 -0400

    Metodos Reserva + cargar productos

---
+++ mystore/app/controllers/categories_controller.rb
@@ -0,0 +1,74 @@
+class categorysController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+  # GET /categorys
+  # GET /categorys.json
+  def index
+    @categories = category.all
+  end
+
+  # GET /categorys/1
+  # GET /categorys/1.json
+  def show
+  end
+
+  # GET /categorys/new
+  def new
+    @category = category.new
+  end
+
+  # GET /categorys/1/edit
+  def edit
+  end
+
+  # POST /categorys
+  # POST /categorys.json
+  def create
+    @category = category.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to @category, notice: 'category was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @category }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /categorys/1
+  # PATCH/PUT /categorys/1.json
+  def update
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to @category, notice: 'category was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /categorys/1
+  # DELETE /categorys/1.json
+  def destroy
+    @category.destroy
+    respond_to do |format|
+      format.html { redirect_to categories_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_category
+      @category = category.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def category_params
+      params.require(:category).permit(:name, :product_id)
+    end
+end
