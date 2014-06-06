f1556a61fca9c261c3497e04c9c1e75ee8a17f71
sacharad <sebastiancharad@gmail.com>
2014-06-06 17:38:23 -0400

    Created method actualizarAlmacenes

--- mystore/db/schema.rb
+++ mystore/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140520192735) do
+ActiveRecord::Schema.define(version: 20140606195909) do
+
+  create_table "almacens", force: true do |t|
+    t.string   "name"
+    t.string   "almacen_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "autorizacions", force: true do |t|
     t.string   "grupo"
