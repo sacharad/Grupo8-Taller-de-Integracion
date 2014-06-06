1b0af3c1a0bc243af9a3e3a409b8010adf42a188
sacharad <sebastiancharad@gmail.com>
2014-06-06 18:01:13 -0400

    comment in Almacen

--- mystore/app/models/almacen.rb
+++ mystore/app/models/almacen.rb
@@ -1,4 +1,9 @@
 class Almacen < ActiveRecord::Base
+  # Metodo para buscar por nombre de almacen. Nombres pueden ser:
+  # => general
+  # => pulmon
+  # => despacho
+  # => recepcion
   def self.buscar(almacen_name)
     Almacen.find_by_name(almacen_name)
   end
