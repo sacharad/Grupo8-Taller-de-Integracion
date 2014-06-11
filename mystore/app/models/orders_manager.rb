eeb260b99fb94212d674b984d05b78c05517f598
acathali <acathali@uc.cl>
2014-06-11 16:10:54 -0400

    Ajustes OrdersManager entrega 3

--- mystore/app/models/orders_manager.rb
+++ mystore/app/models/orders_manager.rb
@@ -1,7 +1,5 @@
 class OrdersManager < ActiveRecord::Base
 
-### ESTO NO ESTA LISTO, NO VA A FUNCIONAR
-
 	def self.fetchOrders
 		ftp = Connectors::SftpConnector.new
 		vtiger = Connectors::VtigerConnector.new
@@ -19,78 +17,92 @@ class OrdersManager < ActiveRecord::Base
 		pedidos_ftp.each do |p| #asumo son pedidos nuevos/no resueltos
 			Rails.logger.info p.to_s
 			p = JSON.parse p
-			Rails.logger.info "1 pedido['Pedidos']"+p['Pedidos'].to_s
+			Rails.logger.info "1 pedido['Pedidos']" + p['Pedidos'].to_s
+			#EN WINDOWS CAMBIAR ESTO POR: p[1]
 			pedido = p["Pedidos"]
-			Rails.logger.info "2 pedido['Pedidos']"+pedido.to_s
+			Rails.logger.info "2 pedido['Pedidos']" + pedido.to_s
 			vtiger_address = vtiger.getAddress(pedido["direccionId"])
 			if vtiger_address.nil?
 				Rails.logger.info 'ERROR self.fetchOrders in OrdersManager, received nil in vtiger.getAddress(pedido["direccionID"]) execution. line 25'
 				vtiger_address = "Direccion Invalida"
 			end
 
-			Order.report_order(pedido)
+			#~~~~~ Order.report_order(pedido) -->
 			reporte = {
 				"pedidoID" => pedido["pedidoID"], 
 				"fecha" => pedido["fecha"],
 				"hora" => pedido["hora"],
 				"direccion" => vtiger_address,
-				"rut" => pedido["rut"]
+				"rut" => pedido["rut"],
+				"pedidos" => []
 			}
+			reporte_quiebre = reporte.dup
 			if vtiger.checkClient(pedido["direccionID"], pedido["rut"])
 				quiebre = false
 
-				reporte["pedidos"] = []			
 				pedido["Pedido"].each do |pedidos|
+					# ::> Revisar skus, generar reportes de quiebre y de despacho
 					rep_indv = checkPedido(pedido["rut"], pedidos)
-					quiebre = true if rep_indv["status"] == "quiebre"
-					reporte["pedidos"] << rep_indv
-				end
-				unless quiebre
-					reporte["pedidos"].each do |rep|
-						rep.except!("status")
-
-						productos = @warehouse.getStock(ENV["ALMACEN_LIBRE_DISPOSICION"], rep["sku"])
-						productos = productos.nil? ? 0 : productos.take(rep["cantidad"].to_i)
+					if rep_indv["status"] == "quiebre"
+						quiebre = true
+						reporte_quiebre["pedidos"] << rep_indv
+					else
+						# ::> Despachar
+						productos = @warehouse.getStock(Almacen.buscar("general")["almacen_id"], rep_indv["sku"])
+						productos = productos.nil? ? 0 : productos.take(rep_indv["cantidad"].to_i)
 
 						productos.each do |producto|
-							@warehouse.realizarDespacho(producto["_id"], reporte["direccion"], rep["precio"].to_i, pedido["pedidoID"])
+							@warehouse.realizarDespacho(producto["_id"], reporte["direccion"], rep_indv["precio"].to_i, pedido["pedidoID"])
 						end
-						Reserve.usarReserva(pedido["rut"], rep["sku"].to_i, [rep["reserva"].to_i, rep["cantidad"].to_i].min)
+						Reserve.usarReserva(pedido["rut"], rep_indv["sku"].to_i, [rep_indv["reserva"].to_i, rep_indv["cantidad"].to_i].min)
+						reporte["pedidos"] << rep_indv
 					end
-					Order.report_sales(reporte)	
-				else
-					Order.report_brokestock(reporte)
 				end
+				Rails.logger.info "Following report is for successful order delivery"
+				Rails.logger.info reporte
+				Rails.logger.info "Following report is for broken stock" if quiebre
+				Rails.logger.info reporte_quiebre if quiebre
+				#~~~~~ Order.report_sales(reporte) -->
+				#~~~~~ Order.report_brokestock(reporte_quiebre) if quiebre -->
 			else
-				Order.report_wrongorder(reporte)
+				#~~~~~ Order.report_wrongorder(reporte) -->
 			end
-			puts "reporte final: #{reporte}"
 		end
 	end
 
 	def self.checkPedido (rut, pedidos)
 		
-		report = {}
-		report["sku"] = pedidos["sku"]
-		report["cantidad"] = pedidos["cantidad"]
+		# ::> checkear la bodega y la reserva del pedido, retornar un reporte para el estado del
+		#     pedido para este sku
+
 
 		stock = @warehouse.obtenerStock(pedidos["sku"])
-		res = stock == 0 ? 0 : Reserve.getReservas(pedidos["sku"].to_i) 
-		r = Reserve.getReserva(rut, pedidos["sku"].to_i)
-		single_res = pedidos["cantidad"].to_i > stock ? 0 : Reserve.getReserva(rut, pedidos["sku"].to_i)
+		total_res = Reserve.getReservas(pedidos["sku"].to_i)
+		reserva_cliente = Reserve.getReserva(rut, pedidos["sku"].to_i)
 
-		unless pedidos["cantidad"].to_i <=  stock - res + single_res
-			report["status"] = "quiebre"
-			@warehouse.pedirOtraBodega(pedidos["sku"], (pedidos["cantidad"].to_i - stock)) if res == 0
-			return report
+		res = [stock, total_res].min
+		single_res = [stock, reserva_cliente].min
+
+		report = {
+			"sku" => pedidos["sku"],
+			"cantidad" => pedidos["cantidad"],
+			"reserva" => single_res,
+			#~~~~~ --X
+			"precio" => 0
+			#~~~~~ "precio" => Price.get_prices(pedidos["sku"]) -->
+		}
+
+		stock_dif = pedidos["cantidad"].to_i - stock + res - single_res
+		unless stock_dif <=  0
+			Rails.logger.info "Failed to supply order. Status for sku: stock #{stock}, Total reserve #{res}, Client reserve #{single_res}"
+			extra = @warehouse.pedirOtraBodega(pedidos["sku"], stock_dif)[:cantidad_recibida]
+			unless stock_dif - extra <= 0
+				Rails.logger.info "Failed to retrieve enough stock from other warehouses. Needed #{stock_dif}, got #{extra}"
+				report["status"] = "quiebre"
+				return report
+			end
 		end
-
-		report["reserva"] = single_res
-
-		report["precio"] = Price.get_prices(pedidos["sku"])
 		report["status"] = "OK"
-
-
 		return report
 	
 	end
