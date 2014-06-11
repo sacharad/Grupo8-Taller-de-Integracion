7dacbd2aabcea1a4e32f0c3661c1827c8f9f4679
acathali <acathali@uc.cl>
2014-06-11 16:10:54 -0400

    Ajustes OrdersManager entrega 3

--- mystore/app/models/connectors/sftp_connector.rb
+++ mystore/app/models/connectors/sftp_connector.rb
@@ -8,6 +8,11 @@ class Connectors::SftpConnector
 
 		json_pedidos_nuevos = []
 
+
+		#~~~~~ --X
+		i = 0
+		#~~~~~ --X
+
 		#Si ya está en mi base de datos, ya lo procesamos
 		@sftpConn.dir.foreach("/home/grupo8/Pedidos") do |entry|
 	   		if(!OrdersSftp.exists?(:name => entry.name) && entry.name.length > 3)
@@ -17,43 +22,22 @@ class Connectors::SftpConnector
 	   			hash = JSON.parse(data_json)
 			   	fecha = hash["xml"]["Pedidos"]["fecha"][1]
 
-			   	if Date.new(fecha[0..3].to_i,fecha[5..6].to_i,fecha[8..9].to_i)<=Date.today
-			   	#if Date.new(fecha[0..3].to_i,fecha[5..6].to_i,fecha[8..9].to_i)<=Date.today
+			   	if Date.new(fecha[0..3].to_i,fecha[5..6].to_i,fecha[8..9].to_i) <= Date.today
 		   			pedidos = hash["xml"]
 		   			pedidos["Pedidos"]["pedidoID"] = entry.name[7..-5]
 		   			json_pedidos_nuevos.push(pedidos.to_json.delete(' ')) 
-		   			OrdersSftp.create(:name => entry.name)
+		   			#~~~~~ OrdersSftp.create(:name => entry.name) {TEMP} -->
 		   		end
-	   		else
-	   			puts entry.name + " exists"
 	   		end
+	   		#~~~~~ --X
+	   		i == 10 ? break : i += 1
+	   		#~~~~~ --X
 	    end
 
-	    # direccion = "/home/grupo8/Pedidos/"
-	   	# data = @sftpConn.download!("#{direccion}pedido_977.xml")
-	   	# #data2 = @sftpConn.download!("#{direccion}pedido_1234.xml")
-	   	# data_json = Hash.from_xml(data).to_json
-	   	# hash = JSON.parse(data_json.delete(' '))
-	   	# puts "--------------"
-	   	# fecha = hash["xml"]["Pedidos"]["fecha"][1]
-	   	# puts Date.new(fecha[0..3].to_f,fecha[5..6].to_f,fecha[8..9].to_f)
-	   	# puts Time.now.strftime("%Y-%m-%d")
-	   	# puts Date.new(fecha[0..3].to_f,fecha[5..6].to_f,fecha[8..9].to_f)>Date.today
-	   	# puts "--------------"
-	   	# pedidos = hash["xml"]
-	   	# pedidos["Pedidos"]["pedidoID"] = "977"
-
-
-	   	#puts json_pedidos_nuevos 
-			if json_pedidos_nuevos.first.nil?
-				return nil
-			else
-				return json_pedidos_nuevos
-			end
-	   	
-
-
+		if json_pedidos_nuevos.first.nil?
+			return nil
+		else
+			return json_pedidos_nuevos
+		end
 	end
-
-
 end
